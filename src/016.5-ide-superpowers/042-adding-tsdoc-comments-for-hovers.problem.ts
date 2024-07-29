5874f9c57a6db2df06add1d5f0aafd4c24e58ac7
rsandtner <rsandtner@apache.org>
2024-07-29 23:03:26 +0200

    solved exercises

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,13 @@
+/**
+ * myFunction returns the result of a + b
+ * @param a
+ * @param b
+ *
+ * @returns sum of a + b
+ *
+ * @example
+ * myFunction(1, 2)
+ */
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
